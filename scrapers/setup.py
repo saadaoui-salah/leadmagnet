cc4c3c999e69eaf2a2d5cd2189dc19314f19b89f
saadaoui-salah <salahsaadaoui8@gmail.com>
2026-06-29 23:54:30 +0100

    update

--- scrapers/setup.py
+++ scrapers/setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="myproject",
-    version="1.0",
+    name="core",
+    version="1.0.0",
     packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         line.strip()
         for line in open("requirements.txt")
